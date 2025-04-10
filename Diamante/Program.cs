37f282e6635e2e400f8c916b757b36706dd6f0c7
Carlos Vinicius <kakavinicius83@gmail.com>
2025-04-10 18:34:49 -0300

    Feita a parte de escrever a linha de X para o diamante

--- Diamante/Program.cs
+++ Diamante/Program.cs
@@ -20,4 +20,11 @@
 
         return numero;
     }
-    
+    static void EscreverLinha(int larguraTotal, int linhaAtual)
+    {
+        int quantidadeDeX = linhaAtual * 2 + 1;
+        int espacos = (larguraTotal - quantidadeDeX) / 2;
+
+        Console.Write(new string(' ', espacos));
+        Console.WriteLine(new string('X', quantidadeDeX));
+    }
