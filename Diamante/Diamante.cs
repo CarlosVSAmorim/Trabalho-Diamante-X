2603830d2e7e453aa98badbdf9cf921d47748e8d
alexandre.rech.lages <alexandre.rech.lages@gmail.com>
2025-04-10 21:29:32 -0300

    finalizado a refatoração

---
+++ Diamante/Diamante.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamante
+{
+    internal static class DiamanteDeX
+    {
+
+        public static void DesenharDiamante(int tamanho)
+        {
+            int meio = tamanho / 2;
+
+            // Parte superior
+            for (int i = 0; i <= meio; i++)
+            {
+                EscreverLinha(tamanho, i);
+            }
+
+            // Parte inferior
+            for (int i = meio - 1; i >= 0; i--)
+            {
+                EscreverLinha(tamanho, i);
+            }
+        }
+
+        static void EscreverLinha(int tamanho, int linhaAtual)
+        {
+            int quantidadeDeX = linhaAtual * 2 + 1;
+            int espacos = (tamanho - quantidadeDeX) / 2;
+
+            string margem = new string(' ', espacos);
+            Console.Write(margem);
+
+            string linhaDeX = new string('X', quantidadeDeX);
+            Console.WriteLine(linhaDeX);
+        }
+    }
+}
--- Diamante/Program.cs
+++ Diamante/Program.cs
@@ -1,44 +1,23 @@
-﻿        int numero = SolicitarNumeroImpar();
-        DesenharDiamante(numero);
+﻿using Diamante;
 
-    static int SolicitarNumeroImpar()
+int numero = SolicitarNumeroImpar();
+DiamanteDeX.DesenharDiamante(numero);
+
+int SolicitarNumeroImpar()
+{
+    int numero;
+    do
     {
-        int numero;
-        do
+        Console.Write("Digite um número ímpar: ");
+        bool ehValido = int.TryParse(Console.ReadLine(), out numero);
+
+        if (!ehValido || numero % 2 == 0)
         {
-            Console.Write("Digite um número ímpar: ");
-            bool ehValido = int.TryParse(Console.ReadLine(), out numero);
-
-            if (!ehValido || numero % 2 == 0)
-            {
-                Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro ímpar.");
-            }
-
-        } while (numero % 2 == 0);
-
-        return numero;
-    }
-    static void EscreverLinha(int larguraTotal, int linhaAtual)
-    {
-        int quantidadeDeX = linhaAtual * 2 + 1;
-        int espacos = (larguraTotal - quantidadeDeX) / 2;
-
-        Console.Write(new string(' ', espacos));
-        Console.WriteLine(new string('X', quantidadeDeX));
-    }
-    static void DesenharDiamante(int tamanho)
-    {
-        int meio = tamanho / 2;
-
-        // Parte superior
-        for (int i = 0; i <= meio; i++)
-        {
-            EscreverLinha(tamanho, i);
+            Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro ímpar.");
         }
 
-        // Parte inferior
-        for (int i = meio - 1; i >= 0; i--)
-        {
-            EscreverLinha(tamanho, i);
-        }
-    }
+    } while (numero % 2 == 0);
+
+    return numero;
+}
+
